d2fc442a6bc9ab070a1b0be43bbdd4ae4f71fbf6
Philipp Stracker <p.stracker@syde.com>
2025-09-21 21:33:39 +0200

    ✨ Dummy plugin file

---
+++ whiskey.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Whiskey
+ * Description: WordPress configuration recipes. Like fine whiskey, code gets better with age.
+ * Version: 1.0.0
+ * Requires at least: 5.4
+ * Requires PHP: 7.4
+ * Author: Philipp Stracker (p.stracker@syde.com)
+ * License: MIT
+ *
+ * @package Whiskey
+ */
+
+defined( 'ABSPATH' ) || exit;
