098d19f1d99f50a9d13117b849181572c3e566d7
Philipp Stracker <p.stracker@syde.com>
2025-10-18 00:06:03 +0200

    🔧 Extend the default recipe collection

--- src/Recipes/default.php
+++ src/Recipes/default.php
@@ -1,38 +1,73 @@
 <?php
 /**
- * Recipes for: PayPal Payments for WooCommerce
+ * Default recipes.
  */
 
 declare( strict_types = 1 );
 
 use Whiskey\Registry\RecipeRegistry;
-use Whiskey\Ingredients\SetHomepageIngredient;
 use Whiskey\Ingredients\CreatePagesIngredient;
+use Whiskey\Ingredients\SetHomepageIngredient;
+use Whiskey\Ingredients\SetActiveThemeIngredient;
+use Whiskey\Ingredients\SetMenuItemsIngredient;
+use Whiskey\Ingredients\UpdatePermalinksIngredient;
+use Whiskey\Ingredients\SetWooCountryIngredient;
+use Whiskey\Ingredients\SetWooCurrencyIngredient;
 
 add_action( 'whiskey:register_recipe', static function ( RecipeRegistry $registry ) {
 	$registry->add(
-		'default-shop',
+		'site-setup',
 		[
-			CreatePagesIngredient::NAME => [
+			// Create all 6 WooCommerce pages
+			CreatePagesIngredient::NAME      => [
 				'shop',
 				'classic-cart',
+				'block-cart',
 				'classic-checkout',
+				'block-checkout',
+				'my-account',
 			],
-			SetHomepageIngredient::NAME     => 'shop',
+
+			// Set Storefront as active theme
+			SetActiveThemeIngredient::NAME   => 'storefront',
+
+			// Set shop page as homepage
+			SetHomepageIngredient::NAME      => 'shop',
+
+			// Add pages to primary menu
+			SetMenuItemsIngredient::NAME     => [
+				'shop',
+				'classic-cart',
+				'block-cart',
+				'classic-checkout',
+				'block-checkout',
+				'my-account',
+			],
+
+			// Set pretty permalinks
+			UpdatePermalinksIngredient::NAME => '/%postname%/',
 		]
 	);
 
 	$registry->add(
-		'german_merchant',
+		'us-shop',
 		[
-			// TODO
+			// Set store location to California, US
+			SetWooCountryIngredient::NAME  => 'US:CA',
+
+			// Set currency to USD
+			SetWooCurrencyIngredient::NAME => 'USD',
 		]
 	);
 
 	$registry->add(
-		'mexico_merchant',
+		'eu-shop',
 		[
-			// TODO
+			// Set store location to Austria
+			SetWooCountryIngredient::NAME  => 'AT',
+
+			// Set currency to EUR
+			SetWooCurrencyIngredient::NAME => 'EUR',
 		]
 	);
 } );
